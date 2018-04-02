26d95b855969460aaf3027c76b9dab3f6c0ad288
simbo1905 <simbo1905@60hertz.com>
2018-04-02 22:17:01 +0100

    openshift template

---
+++ create-openshift.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SOURCE_REPOSITORY_REF=$(git status | awk 'NR==1{print $NF}')
+SOURCE_REPOSITORY_URL=$(git remote show origin | awk 'NR==2{print $NF}')
+if [[ -n "$SOURCE_REPOSITORY_REF" ]]; then
+  NAMESPACE=$(oc project --short)
+  echo git url is $SOURCE_REPOSITORY_URL
+  echo git branch is $SOURCE_REPOSITORY_REF
+  oc process -f openshift-template.json  \
+    -p NAME=$NAME \
+    -p NAMESPACE=$NAMESPACE \
+    -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL \
+    -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
+  | oc create -f -	
+else
+  echo "git status didn't resolve a git branch"
+  exit 1
+fi
